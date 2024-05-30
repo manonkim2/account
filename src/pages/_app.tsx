8aed1eb2d88771f0fa092235744b53442513602f
Manon Kim <kim.manon2@gmail.com>
2024-05-30 21:19:06 +0900

    setting

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from 'next/app'
+import { Global } from '@emotion/react'
+
+import globalStyles from '@/styles/globalStyles'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Global styles={globalStyles} />
+      <Component {...pageProps} />
+    </>
+  )
+}
