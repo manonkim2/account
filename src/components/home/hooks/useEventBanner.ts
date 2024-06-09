9c009e6d767b167482669ac3d11df0cc610be1c0
Manon Kim <kim.manon2@gmail.com>
2024-06-09 14:13:35 +0900

    [feat] home - account

--- src/components/home/hooks/useEventBanner.ts
+++ src/components/home/hooks/useEventBanner.ts
@@ -1,12 +1,21 @@
-import { getEventBanners } from '@/remote/banner'
 import { useQuery } from 'react-query'
 
-const useEventBanner = () => {
+import { getEventBanners } from '@remote/banner'
+import useAccount from '@/hooks/useAccount'
+
+function useEventBanners() {
+  const { data: account } = useAccount()
+
   return useQuery(
     ['event-banners'],
-    () => getEventBanners({ hasAccount: false }),
-    { suspense: true },
+    () =>
+      getEventBanners({
+        hasAccount: account != null && account.status === 'DONE',
+      }),
+    {
+      suspense: true,
+    },
   )
 }
 
-export default useEventBanner
+export default useEventBanners
